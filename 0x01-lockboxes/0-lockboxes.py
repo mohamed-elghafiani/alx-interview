c5f5fc0d1e95ca9f90ea61ec60a322a092c9c7af
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-03-23 12:21:23 +0000

    solved

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""alx-interview
+   Task: Lockboxes
+"""
+
+
+def open_recursive(unlocked, start, boxes):
+    """Open boxes recursively"""
+    if not start:
+        return
+    for i in start:
+        if i < len(boxes) and i not in set(unlocked):
+            unlocked.append(i)
+            open_recursive(unlocked, boxes[i], boxes)
+        else:
+            pass
+
+    return unlocked
+
+def canUnlockAll(boxes):
+    """Check if all boxes can be unlocked"""
+    unlocked = [0]
+    unlocked = open_recursive(unlocked, boxes[0], boxes)
+
+    for i in range(len(boxes)):
+        if i not in set(unlocked):
+            return False
+    return True
+
