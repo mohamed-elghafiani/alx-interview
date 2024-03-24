a28aaa45ac8f6ca684d4be394fcf2d04d07020df
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-03-24 21:26:01 +0000

    best solution

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,46 +4,17 @@
 """
 
 
-def find_num_op(n, text, op, paste, nop, solutions):
-    """Find the number of operations possible"""
-    if len(text) >= n:
-        solutions.append((text, nop))
-        return (text, nop)
-    solutions = solutions
-    if op == 'P':
-        text += paste
-        nop += 1
-
-        find_num_op(n, text, 'P', paste, nop, solutions)
-        find_num_op(n, text, 'CP', paste, nop, solutions)
-
-    elif op == 'CP':
-        paste = text
-        text += paste
-        nop += 2
-
-        find_num_op(n, text, 'P', paste, nop, solutions)
-        find_num_op(n, text, 'CP', paste, nop, solutions)
-
-    return solutions
-
-
 def minOperations(n):
-    """Find minimum operations needed to arrive at n * 'H' """
-    if n > 1:
-        solutions = find_num_op(
-                n,
-                text='H',
-                op='CP',
-                paste='',
-                nop=0,
-                solutions=[]
-                )
-        finals = list(filter(lambda item: len(item[0]) == n, solutions))
-        if finals:
-            best = min(finals, key=lambda item: item[1])
-            return best[1]
-        else:
-            return 0
-    else:
-        return 0
+    """Find minimum operations needed to arrive at n H's
+    """
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n /= factor
+
+        factor += 1
+
+    return operations
