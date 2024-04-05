a303d41cd200e1ef890c37b5a2944eb6119f0e6d
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-04-04 18:06:20 -0700

    completed task

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""ALx Interview Questions
+   UTF-8 Validation module
+"""
+
+
+def encoding_bytes(value: int) -> int:
+    """Determines the number of bytes used to encode a character
+       based on the first byte value @value
+
+       Args:
+         value: an integer representing the value of the first byte
+                of  a chain of chars
+
+       Returns:
+         the number of bytes used to encode the char accoring to UTF-8
+         encoding standard
+         returns -1 if invalid
+    """
+    if value & int('10000000', 2) == 0:
+        return 1
+    elif value & int('11100000', 2) == int('11000000', 2):
+        return 2
+    elif value & int('11110000', 2) == int('11100000', 2):
+        return 3
+    elif value & int('11111000', 2) == int('11110000', 2):
+        return 4
+    else:
+        return -1
+
+
+def validUTF8(data):
+    """Checks if a given data set represents a valid UTF-8 encoding"""
+    i = 0
+    while i < len(data):
+        skip = encoding_bytes(data[i])
+        if skip == -1:
+            return False
+        for i in range(1, skip):
+            if data[i] & int('11000000', 2) != int('10000000', 2):
+                return False
+            i += 1
+        i += skip
+    return True
