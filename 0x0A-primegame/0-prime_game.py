693a768f281503c3170f93c974763ac13636eab5
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-08-10 07:12:39 -0700

    pep8 styling

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,13 +4,15 @@
 
 def sieve_of_eratosthenes(max_n):
     """
-    Generates a list of boolean values indicating whether each number up to max_n is a prime number.
+    Generates a list of boolean values indicating whether
+    each number up to max_n is a prime number.
 
     Parameters:
     max_n (int): The upper limit of numbers to check for primality.
 
     Returns:
-    list: A list of boolean values where the index represents the number, and the value is True if it's a prime.
+    list: A list of boolean values where the index represents
+    the number, and the value is True if it's a prime.
     """
     is_prime = [True] * (max_n + 1)
     is_prime[0] = is_prime[1] = False
@@ -21,17 +23,21 @@ def sieve_of_eratosthenes(max_n):
                 is_prime[j] = False
     return is_prime
 
+
 def isWinner(x, nums):
     """
-    Determines the winner of a series of rounds in a game where Maria and Ben take turns removing prime numbers
+    Determines the winner of a series of rounds in a game where
+    Maria and Ben take turns removing prime numbers
     and their multiples from a set of integers.
 
     Parameters:
     x (int): The number of rounds to be played.
-    nums (list): A list of integers where each integer represents the upper limit of the set for that round.
+    nums (list): A list of integers where each integer represents
+    the upper limit of the set for that round.
 
     Returns:
-    str: The name of the player that won the most rounds ("Maria" or "Ben"), or None if the winner cannot be determined.
+    str: The name of the player that won the most rounds
+    ("Maria" or "Ben"), or None if the winner cannot be determined.
     """
     if not nums or x <= 0:
         return None
@@ -61,4 +67,3 @@ def isWinner(x, nums):
         return "Ben"
     else:
         return None
-
